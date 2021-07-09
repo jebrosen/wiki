861d05a3b4556ee6306572f44fb0b9f24cc29ca3
Jeb Rosen <jeb@jebrosen.com>
2021-07-09 15:30:29 -0700

    Change dropdown selector in BetterTable component to a text input.

--- client/themes/default/components/better-table.vue
+++ client/themes/default/components/better-table.vue
@@ -18,20 +18,14 @@ this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
         | {{column}}
         button(v-on:click='changeSort(column)')
           | {{ sortBy === column ? (sortDir === 1 ? "⬇" : "⬆") : "↕" }}
-        select(v-if='shouldFilter(colIndex)' v-on:change='changeFilter(colIndex, $event.target.value)')
-          option(value='<Any>')
-            | &lt;Any&gt;
-          option(v-for='value in data.uniqueValues[colIndex]' v-bind:value='value')
-            | {{value}}
+        input(type='text' placeholder='Find...'
+              v-on:input='changeFilter(colIndex, $event.target.value)')
     tr(v-for='row in visibleRows')
       td(v-for='cell in row')
-        template(v-if='cell.html')
-          //- This usage of v-html is safe because the same html was already
-          //- present elsewhere on the page (and has already been sanitized
-          //- etc. by the rendering pipeline)
-          span(v-html='cell.html')
-        template(v-else)
-          | {{cell.text}}
+        //- This usage of v-html is safe because the same html was already
+        //- present in a table cell (and has already been sanitized
+        //- etc. by the rendering pipeline)
+        span(v-html='cell.html')
 </template>
 
 <script>
@@ -42,46 +36,20 @@ this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 function extractData(table) {
   const columns = [];
   const rows = [];
-  const uniqueValues = [];
 
   table.querySelectorAll("tr").forEach((row, rowIndex) => {
     if (rowIndex === 0) {
       // header row
       row.querySelectorAll("th, td").forEach(td => {
         columns.push(td.innerText);
-        uniqueValues.push([]);
       });
     } else {
       const cells = [];
       row.querySelectorAll("th, td").forEach((td, colIndex) => {
-        let text = td.innerText;
-        let values;
-        let html = null;
-        // Don't want to mess up e.g. hyperlinks by splitting them;
-        // for a simple heuristic any cell with child HTML elements
-        // is assumed to be HTML.
-        if (td.children.length > 0) {
-          values = [text];
-          html = td.innerHTML;
-        } else {
-          // split by commas,
-          // ignore any empty values appearing in the middle of the cell
-          // (probably due to extraneous commas)
-          values = text.split(/\s*,\s*/).filter(v => v.length > 0);
-        }
+        const textLower = td.innerText.toLowerCase();
+        const html = td.innerHTML;
 
-        // if no other values were present, treat as a single empty value
-        // (convenient for filtering to 'blank' rows)
-        if (values.length === 0) {
-          values.push('');
-        }
-
-        values.forEach(v => {
-          if (uniqueValues[colIndex].indexOf(v) === -1) {
-            uniqueValues[colIndex].push(v);
-          }
-        });
-        cells.push({ text, values, html });
+        cells.push({ textLower, html });
       });
       rows.push(cells);
     }
@@ -89,9 +57,7 @@ function extractData(table) {
 
   table.style.display = 'none';
 
-  uniqueValues.forEach(v => v.sort());
-
-  return { columns, uniqueValues, rows };
+  return { columns, rows };
 }
 
 export default {
@@ -115,7 +81,8 @@ export default {
       for (const row of this.$data.data.rows) {
         let visible = true;
         this.$data.filters.forEach(filter => {
-          if (row[filter.colIndex].values.indexOf(filter.value) === -1) {
+          const filterVal = filter.value.toLowerCase();
+          if (!row[filter.colIndex].textLower.includes(filterVal)) {
             visible = false;
           }
         })
@@ -129,8 +96,8 @@ export default {
         const sortByIdx = this.$data.data.columns.indexOf(this.$data.sortBy);
         if (sortByIdx !== -1) {
           rows.sort((a, b) => {
-            const val1 = a[sortByIdx].text.toLowerCase();
-            const val2 = b[sortByIdx].text.toLowerCase();
+            const val1 = a[sortByIdx].textLower;
+            const val2 = b[sortByIdx].textLower;
 
             const order = (val1 < val2)
               ? -1
@@ -154,18 +121,6 @@ export default {
         this.$data.sortBy = column;
         this.$data.sortDir = 1;
       }
-    },
-    shouldFilter (colIndex) {
-      if (this.$data.data.uniqueValues[colIndex].length <= 1) {
-        // too few values for a filter
-        return false;
-      }
-      if (this.$data.data.rows.length > 0 && (this.$data.data.uniqueValues[colIndex].length / this.$data.data.rows.length) > 0.8) {
-        // almost as many filter values as there are total rows;
-        // unlikely to be a useful filter
-        return false;
-      }
-      return true;
     },
     changeFilter (colIndex, value) {
       const existingFilterIdx = this.$data.filters.findIndex(f => f.colIndex === colIndex);
@@ -192,11 +147,16 @@ export default {
 </script>
 
 <style lang="scss">
-.better-table select {
+.better-table input {
   max-width: 10em;
-  -webkit-appearance: auto;
-  -moz-appearance: auto;
-  border-style: initial;
+
+  border-width: 0 0 1px 0;
+  border-color: black;
+  border-style: solid;
+}
+
+.better-table input::placeholder {
+  color: #606060;
 }
 
 .better-table th {
